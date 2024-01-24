f6f41fe728062abbea55087e171229953dc0f8dc
Wonderworlds <fmauguin.pro@gmail.com>
2024-01-24 18:48:42 +0100

    maxprofit

--- maxProfit.py
+++ maxProfit.py
@@ -1,17 +1,16 @@
 class Solution:
     def maxProfit(self, prices: list[int]) -> int:
-        delta = 0
-        start = None
-        if prices is None or len(prices) < 2:
-            return 0
-        for x in range(0, len(prices) - 1):
-            if (start is not None) and (x < start):
-                continue
-            for y in range(x + 1, len(prices)):
-                if prices[y] - prices[x] > delta:
-                    delta = prices[y] - prices[x]
-                    start = x
-        print(delta)
+        left = 0  # Buy
+        right = 1  # Sell
+        max_profit = 0
+        while right < len(prices):
+            currentProfit = prices[right] - prices[left]  # our current Profit
+            if prices[left] < prices[right]:
+                max_profit = max(currentProfit, max_profit)
+            else:
+                left = right
+            right += 1
+        return max_profit
 
 
 solution = Solution()
